dbbb9bcf221fe688a29ba3c020e82a03c3cde554
athul.a <athul.a@zerone-consulting.com>
2024-12-27 17:39:35 +0530

    add loading state and display fetched data in about component

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/about-data-service/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent implements OnInit {
   aboutData: any = [];
+  dataLoading: boolean = false;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataLoading = true;
     this.dataService.getDataService().subscribe((data) => {
       this.aboutData = data;
+      this.dataLoading = false;
     });
-    console.log(this.aboutData);
   }
 }
